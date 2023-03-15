95e4ccc86ef4fb0d901017f03d868333d7b84814
jayshree <jayshreeaher02@gmail.com>
2023-03-15 15:33:59 +0530

    home page imple

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFountComponent } from './core/components/page-not-fount/page-not-fount.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:"**",component:PageNotFountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
