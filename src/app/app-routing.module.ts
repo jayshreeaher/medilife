e135ae889081166c3954cc3894970f5afed1822b
jayshree <jayshreeaher02@gmail.com>
2023-03-19 18:44:20 +0530

    top-deals implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
 import { PageNotFountComponent } from './core/components/page-not-fount/page-not-fount.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
+  {path:"medicine-home",component:MedicineHomeComponent},
   {path:"",redirectTo:"/home",pathMatch:"full"},
   {path:"**",component:PageNotFountComponent}
 ];
---
+++ src/app/components/medicine-home/medicine-home.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/core/services/http.service';
+
+@Component({
+  selector: 'app-medicine-home',
+  templateUrl: './medicine-home.component.html',
+  styleUrls: ['./medicine-home.component.scss']
+})
+export class MedicineHomeComponent implements OnInit{
+  pincode:string="";
+  pincodeDetails:any;
+  displayErrorMessage:boolean=false;
+  showDefaultPincode:boolean=true;
+
+  constructor(private http:HttpService){}
+ngOnInit(): void {
+  
+}
+VerifyPincode(){
+  const endPoint="pin-code-details?"+"pincode="+this.pincode;
+  this.http.getDataFromServer(endPoint).subscribe((el:any)=>{
+    if(el && el.length>0){
+      this.pincodeDetails=el[0];
+      this.displayErrorMessage=false;
+      this.showDefaultPincode=false
+    }else{
+      this.displayErrorMessage=true;
+      this.showDefaultPincode=true
+    }
+  })
+}
+}
